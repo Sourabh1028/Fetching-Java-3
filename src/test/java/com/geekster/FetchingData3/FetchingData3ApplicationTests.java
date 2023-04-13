8b9fe998a98a5b8779b8d2eca11bcd25a1d57076
Sourabh Mane <msourabh631@gmail.com>
2023-04-13 15:25:45 +0530

    first commit

---
+++ src/test/java/com/geekster/FetchingData3/FetchingData3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.FetchingData3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingData3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
